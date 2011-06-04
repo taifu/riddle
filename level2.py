b1f9664dc3adb74d193c6dc2c76e1594b02773d0
Marco Beri <marcoberi@gmail.com>
2011-06-05 01:38:07 +0200

    altra idea

--- level2.py
+++ level2.py
@@ -2,3 +2,7 @@
 # E da queste pagine prendere le xesime lettere
 #
 # Trovare qualcosa che non cambi assolutamente
+#
+#
+# Oppure usare i caratteri unicode strani per scrivere comunque
+# una parola tipo lolcat
