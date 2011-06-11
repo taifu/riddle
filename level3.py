8cb5e9ce1ae610305725965c114605a57907730d
Marco Beri <marcoberi@gmail.com>
2011-06-11 18:17:43 +0200

    level 1 and 3

--- level3.py
+++ level3.py
@@ -7,9 +7,14 @@ import this
 sys.stdout = old_out
 new_out.seek(0)
 msg = new_out.read()
-from collections import defaultdict
-pos = defaultdict(list)
-for i, c in enumerate(msg):
-    pos[c].append(i)
-for c, i in sorted(pos.items()):
-    print c, ",".join(str(n) for n in i)[:30]
+#from collections import defaultdict
+#pos = defaultdict(list)
+#for i, c in enumerate(msg):
+#    pos[c].append(i)
+#for c, i in sorted(pos.items()):
+#    print c, ",".join(str(n) for n in i)[:30]
+
+solution = ""
+for i in (11, 104, 82, 212, 183, 67, 198, 73, 668, 3, 540, 236, 16, 855):
+    solution += msg[i]
+print solution
