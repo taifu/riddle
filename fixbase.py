6501e40da65c7adec3236ac45ff83e953ee719eb
Marco Beri <marcoberi@gmail.com>
2011-06-11 13:37:19 +0200

    level 1 done

---
+++ fixbase.py
@@ -0,0 +1,19 @@
+import random
+UPPERCASE = "".join(chr(x) for x in range(ord("A"), ord("A") + 26))
+LOWERCASE = "".join(chr(x) for x in range(ord("a"), ord("a") + 26))
+maze = file("base.txt", "r").readlines()
+for x, line in enumerate(maze):
+    line = list(line)
+    for y, car in enumerate(line):
+        if x > 0 and y > 0 and x % 2 == 0 and y % 2 == 0:
+            letters = UPPERCASE           
+            if car == " ":
+                letters += LOWERCASE           
+            elif car != "!":
+                continue
+            if random.randint(1, 100) > 30:
+                line[y] = random.choice(letters)
+        if line[y] == "!":
+            line[y] = " "
+    maze[x] = "".join(line)
+file("maze.txt", "w").write("".join(maze))
--- level1.py
+++ level1.py
@@ -1,6 +1,7 @@
-PATH = " " + "".join(chr(x) for x in range(ord("a"), ord("a") + 26))
-print PATH
-START, EXIT, VISITED, SOLUTION = 'SE.+'
+START, EXIT, VISITED, SOLUTION = '=>?!'
+UPPERCASE = "".join(chr(x) for x in range(ord("A"), ord("A") + 26))
+LOWERCASE = "".join(chr(x) for x in range(ord("a"), ord("a") + 26))
+PATH = " " + START + LOWERCASE + UPPERCASE + "!"
 class Maze():
     def __init__(self, ascii_maze):
         self.maze = [list(x) for x in ascii_maze.splitlines()]
@@ -10,17 +11,27 @@ class Maze():
     def __repr__(self):
         return "\n".join("".join(line) for line in self.maze)
 
-    def solve(self, x = None, y = None):
-        if x == None:
-            x, y = self.start_x, self.start_y
-        if self.maze[y][x] in (PATH, START):
-            self.maze[y][x] = VISITED
-            if (self.solve(x+1, y) or self.solve(x-1, y) or
-                self.solve(x, y+1) or self.solve(x, y-1)):
-                self.maze[y][x] = SOLUTION
+    def solve(self, x = None, y = None, letters = []):
+        try:
+            if x == None:
+                x, y = self.start_x, self.start_y
+            if self.maze[y][x] in PATH:
+                added = False
+                if self.maze[y][x] in LOWERCASE:
+                    letters.append(self.maze[y][x])
+                    added = True
+                self.maze[y][x] = VISITED
+                if (self.solve(x+1, y, letters) or self.solve(x-1, y, letters) or
+                    self.solve(x, y+1, letters) or self.solve(x, y-1, letters)):
+                    self.maze[y][x] = SOLUTION
+                    return True
+                elif added:
+                    letters.pop(len(letters) - 1)
+            elif self.maze[y][x] == EXIT:
+                self.letters = letters
                 return True
-        elif self.maze[y][x] == EXIT:
-            return True
+        except IndexError:
+            pass
         return False
 
 ASCII_MAZE = """
@@ -44,4 +55,4 @@ if __name__ == "__main__":
     else:
         maze = Maze(ASCII_MAZE)
     if maze.solve():
-        print maze
+        print "".join(maze.letters)
