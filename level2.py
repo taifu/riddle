06278d3a80e1488d1ccc64c8b73bc11e0e361f47
Marco Beri <marcoberi@gmail.com>
2011-06-12 03:55:49 +0200

    level 2 done

--- level2.py
+++ level2.py
@@ -1,8 +1,19 @@
-# Recuperare automaticamente le ennesime pagine da un sito
-# E da queste pagine prendere le xesime lettere
-#
-# Trovare qualcosa che non cambi assolutamente
-#
-#
-# Oppure usare i caratteri unicode strani per scrivere comunque
-# una parola tipo lolcat
+html = """
+    29 293
+    95 89
+    6  163
+    91 1319
+    74 79
+    72 925
+    37 1801
+"""
+numbers = list(int(x) for line in html.split("\n") if line for x in line.strip().split(" ") if x)
+html = ""
+for i in range(len(numbers)/2):
+    a, b = numbers[i*2:i*2+2]
+    html += ("&#%d;" % (a*b))
+# &#8497; &#8455; &#978; &#120029; &#5846;  &#66600; &#66637;
+name = "/tmp/uni.html"
+file(name, "w").write(html)
+import webbrowser
+webbrowser.open(name)
